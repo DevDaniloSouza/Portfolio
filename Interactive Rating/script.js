2ddfc01babbcef8f4acc7fea86e3e9f3b5c8bb28
Danilo P. de Souza <121466843+DevDaniloSouza@users.noreply.github.com>
2023-05-19 22:17:14 -0300

    Alterações e tentativas

--- Interactive Rating/script.js
+++
@@ -1,8 +0,0 @@
-let buttons = document.getElementsByClassName('btn')
-
-let submit = document.getElementById('submit')
-submit.addEventListener('click', function (ev) {
-    ev.preventDefault()
-
-    submit.style.backgroundColor = 'white'
-})
