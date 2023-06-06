5a4f3a9c6769c1bb4902fa8a5ffe220a9629bca8
Danilo P. de Souza <121466843+DevDaniloSouza@users.noreply.github.com>
2023-06-06 17:42:46 -0300

    Updates

---
+++ Interactive Rating/script.js
@@ -0,0 +1,24 @@
+function rated1() {
+    let btn = document.getElementById('rate1')
+    btn.style.backgroundColor = '#fb7413'
+}
+
+function rated2() {
+    let btn = document.getElementById('rate2')
+    btn.style.backgroundColor = '#fb7413'
+}
+
+function rated3() {
+    let btn = document.getElementById('rate3')
+    btn.style.backgroundColor = '#fb7413'
+}
+
+function rated4() {
+    let btn = document.getElementById('rate4')
+    btn.style.backgroundColor = '#fb7413'
+}
+
+function rated5() {
+    let btn = document.getElementById('rate5')
+    btn.style.backgroundColor = '#fb7413'
+}
